19ae40e88ab7e80ca0c1f16a05909bfe7f9d12f9
ziming <ziming@users.noreply.github.com>
2024-01-14 16:13:48 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Ziming\LaravelEmailSniperLink\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Ziming\LaravelEmailSniperLink\LaravelEmailSniperLinkServiceProvider;
 
